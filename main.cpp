e4b74142fd43806c313e6b130f74e1db3315c4db
FelipGC <felipguimeracuevas@hotmail.com>
2020-12-18 13:01:56 +0100

    CSV updated.

--- main.cpp
+++ main.cpp
@@ -476,7 +476,7 @@ vector<string> get_test_files(const char *directory){
 }
 int main(){
     textFile.open("example.csv");
-    textFile << "file, steps, time, sat" << "\n" ;
+    textFile << "file, steps, time[ms], sat" << "\n" ;
     vector<string> paths = get_test_files("../inputs/test/sat");
     vector<string> paths2 = get_test_files("../inputs/test/unsat");
     paths.insert(paths.end(), paths2.begin(), paths2.end());
