520d5a710531e743a12c4f0d74adc152fefad499
Julian Finkenzeller <julian.finkenzeller@outlook.de>
2021-01-09 13:27:05 +0100

    Refactored times and steps plot accordingly to Felip's(no 'e'!) command

--- plot-steps.py
+++ plot-steps.py
@@ -4,11 +4,24 @@ from matplotlib import pyplot as plt
 
 df_steps = pd.read_csv('cmake-build-debug/steps.csv', index_col=0)
 
+
+a = df_steps.to_numpy()
+
+a.sort(axis=1)
+
+for y in range((len(a))-1):
+    for x in range((len(a[0])-1)):
+        if(x>0):
+            a[y,x]=a[y,x]+a[y,x-1]
+
+df_steps.replace(a)
+
+
 df_steps = df_steps.transpose()
 
 sns_plot_steps = sns.lineplot(data=df_steps, markers=True, dashes=False)
 
-sns_plot_steps.set(xlabel='Problems solved', ylabel='Steps')
+sns_plot_steps.set(xlabel='Problems solved', ylabel='Steps', xticks=[5,10,15,20,25,30])
 
 fiugre_steps = sns_plot_steps.get_figure()
 
--- plot-times.py
+++ plot-times.py
@@ -4,11 +4,22 @@ from matplotlib import pyplot as plt
 
 df_times = pd.read_csv('cmake-build-debug/times.csv', index_col=0)
 
+a = df_times.to_numpy()
+
+a.sort(axis=1)
+
+for y in range((len(a))-1):
+    for x in range((len(a[0])-1)):
+        if(x>0):
+            a[y,x]=a[y,x]+a[y,x-1]
+
+df_times.replace(a)
+
 df_times = df_times.transpose()
 
 sns_plot_times = sns.lineplot(data=df_times, markers=True, dashes=False)
 
-sns_plot_times.set(xlabel='Problems solved', ylabel='Computation time [ms]')
+sns_plot_times.set(xlabel='Problems solved', ylabel='Computation time [ms]', xticks=[5,10,15,20,25,30])
 
 fiugre_times = sns_plot_times.get_figure()
 
